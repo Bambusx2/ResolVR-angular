8cde0981d7de2ae2863ce4bdfc5f45429a257935
Bambusx2 <32058514+Bambusx2@users.noreply.github.com>
2025-02-05 15:56:56 +0100

    Navigate to top after redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ✅ Import CommonModule
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/header/header.component";
 import { FooterComponent } from "./core/footer/footer.component";
@@ -15,6 +16,14 @@ import { ParticlesComponent } from './shared/particles/particles.component';
 export class AppComponent {
   title = 'resolvr';
   isScrolled = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    });
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
