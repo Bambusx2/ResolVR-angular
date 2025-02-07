3711ea99bf7b4b4e7a3e2031204699914926209b
Bambusx2 <32058514+Bambusx2@users.noreply.github.com>
2025-02-07 13:05:10 +0100

    Changed services, updated home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
-import { ServicesComponent } from "../services/services.component";
-
 
 @Component({
   selector: 'app-home',
-  imports: [AboutComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
