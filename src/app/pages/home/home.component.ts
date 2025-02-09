592168806e5b968d9b1d67da7abd2f1879fe7551
Ratko <ratkostip@gmail.com>
2025-02-09 14:07:36 +0100

    fixed testimonials cards and added images in teconoliges

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { AboutComponent } from '../about/about.component';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
   standalone: true
 })
-export class HomeComponent {
-  
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('testimonialContainer', { static: false }) testimonialContainer!: ElementRef;
+
+  ngAfterViewInit(): void {
+  }
+
+  moveSlide(direction: number): void {
+    const container = this.testimonialContainer.nativeElement;
+    const card: HTMLElement = container.querySelector('.testimonial-card');
+    const gap = 30;
+    const cardScrollAmount = card ? card.clientWidth + gap : container.clientWidth;
+    container.scrollLeft += direction * cardScrollAmount;
+  }
+
+  isPrevDisabled(): boolean {
+    const container = this.testimonialContainer?.nativeElement;
+    return container ? container.scrollLeft <= 0 : true;
+  }
+
+  isNextDisabled(): boolean {
+    const container = this.testimonialContainer?.nativeElement;
+    if (!container) { return true; }
+    return (container.scrollLeft + container.clientWidth) >= (container.scrollWidth - 5);
+  }
 }
