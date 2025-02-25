1e9e86f43fe09258472c1506ddd48b5c14d65db5
Ratko <ratkostip@gmail.com>
2025-02-25 14:42:28 +0100

    Carousels
    Changed carousels

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -29,6 +29,13 @@ interface TechStack {
   };
 }
 
+interface Partner {
+  name: string;
+  logo: string;
+  alt: string;
+  website?: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -36,7 +43,7 @@ interface TechStack {
   standalone: true,
   imports: [TestimonialsComponent, CommonModule]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   techStack: TechStack[] = [
     { title: 'Kafka', icon: siApachekafka },
     { title: 'Java', icon: siOpenjdk },
@@ -56,4 +63,83 @@ export class HomeComponent {
     { title: 'HTML', icon: siHtml5 },
     { title: 'CSS', icon: siCss3 },
   ];
+
+  partners: Partner[] = [
+    {
+      name: 'Iskratel',
+      logo: 'assets/logos/iskratel.png',
+      alt: 'Iskratel - A Leading European ICT Provider',
+      website: 'https://www.iskratel.com'
+    },
+    {
+      name: 'Kontron',
+      logo: 'assets/logos/kontron.png',
+      alt: 'Kontron - Global Leader in IoT/Embedded Computing Technology',
+      website: 'https://www.kontron.com'
+    },
+    {
+      name: 'Gazprom',
+      logo: 'assets/logos/gazprom.png',
+      alt: 'Gazprom - Global Energy Company',
+      website: 'https://www.gazprom.com'
+    },
+    {
+      name: 'Hotel Park',
+      logo: 'assets/logos/hotel_park1.png',
+      alt: 'Hotel Park - Luxury Accommodation',
+      website: 'https://www.hotelpark.mk'
+    },
+    {
+      name: 'Rimac',
+      logo: 'assets/logos/rimac.svg',
+      alt: 'Rimac - Electric Hypercar Manufacturer',
+      website: 'https://www.rimac-automobili.com'
+    },
+    {
+      name: 'SPAR ICS',
+      logo: 'assets/logos/spar3.png',
+      alt: 'SPAR ICS - Retail Technology Solutions',
+      website: 'https://www.spar-ics.com'
+    },
+    {
+      name: 'Meyer & Meyer',
+      logo: 'assets/logos/mm.svg',
+      alt: 'Meyer & Meyer - Fashion Logistics',
+      website: 'https://www.meyer-meyer.com'
+    },
+    {
+      name: 'University of Goce Delchev',
+      logo: 'assets/logos/ugd.png',
+      alt: 'University of Goce Delchev - Higher Education',
+      website: 'https://www.ugd.edu.mk'
+    },
+    {
+      name: 'Elektro Ljubljana',
+      logo: 'assets/logos/elj.svg',
+      alt: 'Elektro Ljubljana - Energy Distribution',
+      website: 'https://www.elektro-ljubljana.si'
+    },
+    {
+      name: 'Makpetrol',
+      logo: 'assets/logos/makpetrol3.png',
+      alt: 'Makpetrol - Energy Company',
+      website: 'https://www.makpetrol.com.mk'
+    },
+    {
+      name: 'UNICEF',
+      logo: 'assets/logos/unicef.png',
+      alt: 'UNICEF - United Nations Children\'s Fund',
+      website: 'https://www.unicef.org'
+    },
+    {
+      name: 'RimaTerm',
+      logo: 'assets/logos/rimaTerm2.png',
+      alt: 'RimaTerm',
+      website: 'https://www.rimaterm.mk'
+    }
+  ];
+
+  ngAfterViewInit() {
+    // Implementation will be added when needed for partner carousel initialization
+  }
 }
